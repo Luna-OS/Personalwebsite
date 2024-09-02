fff0108ea13dee2d971f4b0a4cb22a589b3e74a6
Luna <alexanderdanielmueller256@proton.me>
2024-09-02 13:41:25 +0200

    Delete script.js

--- personalwebsite/script.js
+++
@@ -1,9 +0,0 @@
-
-if (window.location.hash) {
-    var hash = window.location.hash.substring(1);
-    if (hash === "personalwebsite") {  
-        window.location.hash = "personalwebsitejoa";
-    } else if (hash === "personalwebsitejoa") {
-        window.location.hash = "personalwebsite";
-    }
-}
